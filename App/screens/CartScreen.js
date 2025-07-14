b4f05692129a40a3e752126c60267e9c7260161d
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-14 17:06:15 +0000

    fix: handle empty cart gracefully in CartScreen

--- App/screens/CartScreen.js
+++ App/screens/CartScreen.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: handle empty cart gracefully in CartScreen
-// Applied: 2026-07-13T09:26:29.628Z
+// Applied: 2026-07-13T09:26:30.381Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:26:29.628Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:26:30.381Z' };
