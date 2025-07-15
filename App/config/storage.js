3c4bca52742d986d9b190ccf3d029ff6734d4c1a
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-15 05:28:47 +0000

    refactor: extract storage keys into constants

---
+++ App/config/storage.js
@@ -0,0 +1,10 @@
+export const STORAGE_KEYS = {
+  AUTH_TOKEN: 'auth_token',
+  USER_DATA: 'user_data',
+  DRIVER_PROFILE: 'driverProfile',
+  DRIVER_STATUS: 'driverStatus',
+  TODAY_EARNINGS: 'todayEarnings',
+  CART_DATA: 'cart_data',
+};
+
+export default STORAGE_KEYS;
