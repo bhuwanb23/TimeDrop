3516845618a07af169a51816b87d7530788c322a
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-15 11:48:15 +0000

    test: add unit tests for deliveryController getDeliveries

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for userController
+ * Unit tests for deliveryController
  */
 
-describe('userController Middleware', () => {
+describe('deliveryController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
