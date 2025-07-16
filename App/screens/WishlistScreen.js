27500c8a77638850a8e5830feeed77c870f1b9fe
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-16 00:23:02 +0000

    fix: add missing useNavigation import to WishlistScreen

--- App/screens/WishlistScreen.js
+++ App/screens/WishlistScreen.js
@@ -1,4 +1,4 @@
-// Bug fix: fix: add missing export default to WishlistScreen
-// Applied: 2026-07-13T09:26:33.758Z
+// Bug fix: fix: add missing useNavigation import to WishlistScreen
+// Applied: 2026-07-13T09:26:39.284Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:26:33.758Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:26:39.284Z' };
