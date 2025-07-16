94eb9580c793613a5348c043a8a2405a8a7a4bd0
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-16 00:58:34 +0000

    fix: add null check for delivery coordinates in DeliveryCard

--- App/components/DeliveryCard.js
+++ App/components/DeliveryCard.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: add null check for delivery coordinates in DeliveryCard
-// Applied: 2026-07-13T09:26:37.682Z
+// Applied: 2026-07-13T09:26:38.134Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:26:37.682Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:26:38.134Z' };
