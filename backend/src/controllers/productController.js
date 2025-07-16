b0542b4b52629f415f68ff438cc3e9d089bbb9d6
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-16 04:54:29 +0000

    fix: handle null response in productController getAll

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: handle null response in productController getAll
-// Applied: 2026-07-13T09:26:38.954Z
+// Applied: 2026-07-13T09:26:39.620Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:26:38.954Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:26:39.620Z' };
