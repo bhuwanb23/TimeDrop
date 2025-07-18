84c3eda1b21b54720a51098f769f094fb8ca607f
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-18 22:39:54 +0000

    fix: replace hardcoded color with theme green in MyOrdersScreen

--- App/screens/MyOrdersScreen.js
+++ App/screens/MyOrdersScreen.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: replace hardcoded color with theme green in MyOrdersScreen
-// Applied: 2026-07-13T09:26:35.937Z
+// Applied: 2026-07-13T09:26:46.352Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:26:35.937Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:26:46.352Z' };
