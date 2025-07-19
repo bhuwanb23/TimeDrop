29db33717ae986e276b12a83d3b99a3f6e26daf2
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-19 15:41:39 +0000

    fix: validate order status transitions in orderController

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: validate order status transitions in orderController
-// Applied: 2026-07-13T09:26:45.260Z
+// Applied: 2026-07-13T09:26:52.337Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:26:45.260Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:26:52.337Z' };
