4dd41f2884fc7ef2f6084d2c7fa4db2d7ae02950
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-21 22:08:06 +0000

    test: add unit tests for productController getAll and getById

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for wishlistController
+ * Unit tests for productController
  */
 
-describe('wishlistController Middleware', () => {
+describe('productController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
