9af7ab720357e0375eeafe29b68016c0e697d2bc
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-23 12:11:28 +0000

    fix: sanitize user input in search queries

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
-// Bug fix: fix: prevent race condition in cart checkout
-// Applied: 2026-07-13T09:27:09.681Z
+// Bug fix: fix: sanitize user input in search queries
+// Applied: 2026-07-13T09:27:10.727Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:27:09.681Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:27:10.727Z' };
