89bf8a5db2d2ec807fc2e58777c052518838f7b0
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-25 11:31:59 +0000

    test: add unit tests for orderController create and list

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for productController
+ * Unit tests for orderController
  */
 
-describe('productController Middleware', () => {
+describe('orderController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
