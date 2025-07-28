b673dbaad943e10ff0d53389a1665a038523aa8f
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-28 15:34:29 +0000

    fix: sanitize user input in search queries

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
-// Bug fix: fix: handle network timeout in API service
-// Applied: 2026-07-13T09:27:31.018Z
+// Bug fix: fix: sanitize user input in search queries
+// Applied: 2026-07-13T09:27:32.806Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:27:31.018Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:27:32.806Z' };
