7a37c8302db86338eb9ab8f2764ec8d8d04e4b51
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-01 19:17:23 +0000

    fix: prevent race condition in cart checkout

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: prevent race condition in cart checkout
-// Applied: 2026-07-13T09:27:47.111Z
+// Applied: 2026-07-13T09:27:49.185Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:27:47.111Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:27:49.185Z' };
