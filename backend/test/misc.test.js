5aa9a43e53c05f8a6c62765ee592ea6c67699bbd
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-07 05:38:42 +0000

    test: add unit tests for wishlistController CRUD

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for cartController
+ * Unit tests for wishlistController
  */
 
-describe('cartController Middleware', () => {
+describe('wishlistController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
