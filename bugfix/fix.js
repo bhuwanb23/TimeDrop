b79afef6e33816e59858a79c48425312adf5eb93
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-08 15:46:29 +0000

    fix: correct pagination offset in product listing

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: correct pagination offset in product listing
-// Applied: 2026-07-13T09:28:19.406Z
+// Applied: 2026-07-13T09:28:22.056Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:28:19.406Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:28:22.056Z' };
