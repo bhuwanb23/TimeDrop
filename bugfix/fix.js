ec8ba38ac5a699ae014b832aeb471fb645701e0a
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-16 15:52:31 +0000

    fix: sanitize user input in search queries

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
-// Bug fix: fix: correct pagination offset in product listing
-// Applied: 2026-07-13T09:28:57.086Z
+// Bug fix: fix: sanitize user input in search queries
+// Applied: 2026-07-13T09:28:58.309Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:28:57.086Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:28:58.309Z' };
