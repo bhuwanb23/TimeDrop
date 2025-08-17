9cb66ba5b474f3227fb3c8901543fa82d5af0ae6
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-17 03:45:48 +0000

    test: add unit tests for adminController dashboard stats

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for deliveryController
+ * Unit tests for adminController
  */
 
-describe('deliveryController Middleware', () => {
+describe('adminController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
