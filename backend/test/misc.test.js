31b6c1fe2d34c002c612328e692966cd546d52b3
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-21 21:28:00 +0000

    test: add unit tests for userController profile update

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for deliveryController
+ * Unit tests for userController
  */
 
-describe('deliveryController Middleware', () => {
+describe('userController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
