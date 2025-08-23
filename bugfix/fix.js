2c58c7bead978e39e3db936613c63f2698750106
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-23 21:28:57 +0000

    fix: sanitize user input in search queries

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
-// Bug fix: fix: correct date formatting in order timestamps
-// Applied: 2026-07-13T09:29:31.909Z
+// Bug fix: fix: sanitize user input in search queries
+// Applied: 2026-07-13T09:29:34.182Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:29:31.909Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:29:34.182Z' };
