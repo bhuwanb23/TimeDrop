3b658c4b4ed07f7df7b70e50b2bd5de148b2fcac
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-27 07:06:16 +0000

    fix: correct pagination offset in product listing

--- bugfix/fix.js
+++ bugfix/fix.js
@@ -1,4 +1,4 @@
-// Bug fix: fix: prevent race condition in cart checkout
-// Applied: 2026-07-13T09:29:52.110Z
+// Bug fix: fix: correct pagination offset in product listing
+// Applied: 2026-07-13T09:29:53.005Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:29:52.110Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:29:53.005Z' };
