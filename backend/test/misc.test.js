40e4f91e4d334c312cbd7516324d749eef42d944
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-29 16:18:10 +0000

    test: add unit tests for categoryController

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for deliveryController
+ * Unit tests for categoryController
  */
 
-describe('deliveryController Middleware', () => {
+describe('categoryController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
