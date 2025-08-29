8dd1702fff890f2dbaf5a04a9cb6a9d8b2396f74
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-29 06:21:14 +0000

    test: add unit tests for cartController add and remove

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for deliveryController
+ * Unit tests for cartController
  */
 
-describe('deliveryController Middleware', () => {
+describe('cartController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
