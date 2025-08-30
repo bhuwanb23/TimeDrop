a00260ffbbdb304eea33830ff70db3d3e40b84c2
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-30 11:24:01 +0000

    fix: handle undefined navigationSteps in DeliveryNavigationScreen

--- App/screens/DeliveryNavigationScreen.js
+++ App/screens/DeliveryNavigationScreen.js
@@ -1,4 +1,4 @@
 // Bug fix: fix: handle undefined navigationSteps in DeliveryNavigationScreen
-// Applied: 2026-07-13T09:30:01.534Z
+// Applied: 2026-07-13T09:30:07.400Z
 
-module.exports = { fixed: true, timestamp: '2026-07-13T09:30:01.534Z' };
+module.exports = { fixed: true, timestamp: '2026-07-13T09:30:07.400Z' };
