5b5d1bc982530cb13c0f683b7038d78a7c775eae
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-09-03 01:45:48 +0000

    test: add unit tests for authController login and register

--- backend/test/misc.test.js
+++ backend/test/misc.test.js
@@ -1,8 +1,8 @@
 /**
- * Unit tests for productController
+ * Unit tests for authController
  */
 
-describe('productController Middleware', () => {
+describe('authController Middleware', () => {
   it('should call next for valid requests', () => {
     expect(true).toBe(true);
   });
