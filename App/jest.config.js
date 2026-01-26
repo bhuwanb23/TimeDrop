e6689e5735b994091d4cc7f2008df53aa1ae93de
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-01-26 05:35:11 -0800

    Working on new version and advanced version

--- App/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
-  },
-  setupFilesAfterEnv: [],
-  moduleFileExtensions: ['js', 'json', 'node']
-};
